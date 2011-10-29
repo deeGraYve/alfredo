e1157a073cbd3acd58f427e07bde27f20b27a07a
Dalton Barreto <daltonmatos@gmail.com>
2011-10-28 22:24:00 -0200

    First tests. Basic functional version.

---
+++ alfredo/__init__.py
@@ -0,0 +1,94 @@
+#encoding: utf-8
+
+import sys
+from PyGtalkRobot import GtalkRobot
+
+import plugnplay
+import base64
+
+
+Plugin = plugnplay.Plugin
+
+
+class ICommand(plugnplay.Interface):
+
+  '''
+   Returns true if the command name passed as a paramater
+   matches the command implemented by this interface
+  '''
+  def match_name(self, command):
+    pass
+
+  '''
+   Run the specific command. Returns a string.
+   Receives the username of the sender and any parameters
+   passed on the message, eg:
+
+    sender: process arg0 arg1 arg2
+
+   would be translated to: run(sender, 'process', arg0, arg1, arg2)
+  '''
+  def run(self, user, command, *args):
+    pass
+
+class InvertCommand(Plugin):
+  implements = [ICommand]
+
+
+  def match_name(self, command):
+    return 'inv' == command
+
+  def run(self, user, *args):
+    if len(args) == 0:
+      return "inv command needs at least one argument"
+    return " ".join(s[::-1] for s in args[::-1])
+
+class Base64Command(Plugin):
+
+  implements = [ICommand]
+
+  def match_name(self, command):
+    return command in ('b64e', 'b64d')
+
+  def run(self, user, command=None, *data):
+    if len(data) == 0:
+      return "{0} command needs one argument".format(command)
+    return getattr(self, "_{0}".format(command))(data[0])
+
+
+  def _b64e(self, data):
+    return base64.b64encode(data)
+
+  def _b64d(self, data):
+    return base64.b64decode(data)
+
+
+class Alfredo(GtalkRobot):
+
+
+  def command_01_hello(self, user, message, args):
+    '''hello(?i)'''
+    self.replyMessage(user, "Olá! tudo bem?")
+
+  def command_03_bounce(self, user, message, args):
+    '''bounce(?i)'''
+    self.replyMessage(user, "{0} {1}".format(message, args))
+
+  def command_02_inv(self, user, message, args):
+    '''inv'''
+    msg = message.split(' ')
+    if len(msg) > 1:
+      remainder = " ".join(msg[1:])
+    else:
+      remainder = ""
+    self.replyMessage(user, remainder[::-1])
+
+
+if __name__ == "__main__":
+  bot = Alfredo()
+  bot.setState('available', 'Alfredo is ready to serve')
+  bot.start('alfredo@daltonmatos.com', sys.stdin.readline())
+
+
+
+
---
+++ tests/unit/command_test.py
@@ -0,0 +1,55 @@
+
+
+
+import unittest
+
+from alfredo import *
+
+class InvertCommandTest(unittest.TestCase):
+
+  def setUp(self):
+    self.inv = InvertCommand()
+
+  def test_check_name_matches(self):
+    self.assertTrue(self.inv.match_name('inv'))
+
+  def test_check_parameter_presence(self):
+    self.assertEquals("inv command needs at least one argument", self.inv.run('daltonmatos@gmail.com'))
+
+  def test_invert_one_argument(self):
+    self.assertEquals("oderfla", self.inv.run('user@domain.com', 'alfredo'))
+
+  def test_invert_two_arguments(self):
+    self.assertEquals("reltub oderfla", self.inv.run('user', 'alfredo', 'butler'))
+
+
+
+class Base64EncodeTest(unittest.TestCase):
+
+  def setUp(self):
+    self.b64e = Base64Command()
+
+  def test_check_name_maches(self):
+    self.assertTrue(self.b64e.match_name('b64e'))
+
+  def test_check_parameter_presence(self):
+    self.assertEquals("b64e command needs one argument", self.b64e.run('user', 'b64e'))
+
+  def test_encode(self):
+    self.assertEquals("YWxmcmVkbyBidXRsZXI=", self.b64e.run('user', 'b64e', 'alfredo butler'))
+
+
+class Base64DecodeTest(unittest.TestCase):
+
+  def setUp(self):
+    self.b64d = Base64Command()
+
+  def test_check_name_maches(self):
+    self.assertTrue(self.b64d.match_name('b64d'))
+
+  def test_check_parameter_presence(self):
+    self.assertEquals("b64d command needs one argument", self.b64d.run('user', 'b64d'))
+
+  def test_encode(self):
+    self.assertEquals("alfredo butler", self.b64d.run('user', 'b64d', 'YWxmcmVkbyBidXRsZXI='))
+
