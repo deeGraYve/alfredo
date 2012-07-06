de34ee4dae8517a941deea45f35823b829d81b95
Dalton Barreto <daltonmatos@gmail.com>
2012-07-05 23:35:37 -0300

    Added MANIFEST.in file, fix setup.py

--- setup.py
+++ setup.py
@@ -3,27 +3,24 @@
 from setuptools import setup
 import os
 
-DIRNAME = os.path.dirname(os.path.abspath(__file__))
 
-with open(os.path.join(DIRNAME, 'README.rst')) as f:
-    long_description = f.read()
+BASE_PATH = os.path.dirname(__file__)
 
 setup(
   name="alfredo",
-  version="0.1",
+  version="0.2",
   url="https://github.com/daltonmatos/alfredo",
   license="3-BSD",
   description="Alfredo is a gtalk bot born so serve you.",
   author="Dalton Barreto",
   author_email="daltonmatos@gmail.com",
-  long_description=long_description,
+  long_description=open(os.path.join(BASE_PATH, 'README.rst')).read(),
   packages=['alfredo'],
-  scripts=[os.path.join(DIRNAME, 'script/alfredo'), os.path.join(DIRNAME, 'script/ud')],
-  install_requires = ['plugnplay', 'xmpppy', 'requests', 'beautifulsoup', 'simplejson'],
-  classifiers = [
+  scripts=[os.path.join(BASE_PATH, 'script/alfredo'), os.path.join(BASE_PATH, 'script/ud')],
+  install_requires=['BeautifulSoup==3.2.1', 'plugnplay==0.5.0', 'requests==0.13.2', 'simplejson==2.6.0', 'xmpppy==0.5.0rc1', ],
+  classifiers=[
     "License :: OSI Approved :: BSD License",
     "Operating System :: OS Independent",
     "Programming Language :: Python",
     "Topic :: Software Development :: Libraries :: Application Frameworks"
     ])
-
